f55a4a4fc5265e641100b44f5152f220d7d9f2b6
Pete Miller <miller.pete+commit@gmail.com>
2016-08-22 17:59:59 +0100

    fix: markdown wrapper referenced removed css

--- wrappers/md.js
+++ wrappers/md.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import 'css/markdown-styles.css'
 import DocumentTitle from 'react-document-title'
 import { config } from 'config'
 
@@ -13,7 +12,7 @@ module.exports = React.createClass({
     const post = this.props.route.page.data
     return (
       <DocumentTitle title={`${config.siteTitle} | ${post.title}`}>
-        <div className="markdown">
+        <div>
           <h1>{post.title}</h1>
           <div dangerouslySetInnerHTML={{ __html: post.body }} />
         </div>
