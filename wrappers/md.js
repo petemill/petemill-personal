1619ae7b35d651fcd3086366259a30c20b9d3fb7
Pete Miller <miller.pete+commit@gmail.com>
2016-08-23 18:40:51 +0100

    md: use doc title

--- wrappers/md.js
+++ wrappers/md.js
@@ -11,7 +11,7 @@ module.exports = React.createClass({
   render () {
     const post = this.props.route.page.data
     return (
-      <DocumentTitle title={`${config.siteTitle} | ${post.title}`}>
+      <DocumentTitle title={`${post.title} || ${config.siteTitle}`}>
         <div>
           <h1>{post.title}</h1>
           <div dangerouslySetInnerHTML={{ __html: post.body }} />
