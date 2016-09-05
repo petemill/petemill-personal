07838bb6fbc6f3bfbe4d96542857d9c00943ffe9
Pete Miller <miller.pete+commit@gmail.com>
2016-09-05 16:02:45 +0100

    home: only show a certain number of writing pages and offer list expansion toggle

--- components/ArticleLink/index.js
+++ components/ArticleLink/index.js
@@ -1,5 +1,6 @@
 import React, {Component, PropTypes} from 'react';
 import stylesArticleLink from './article-link.module.scss';
+const classNames = require('classnames');
 import {Link} from 'react-router';
 import { prefixLink } from 'gatsby-helpers';
 
@@ -22,7 +23,7 @@ export default class ArticleLink extends Component {
     const {name, path, description} = this.props;
     //render markup
     return (
-      <div className={stylesArticleLink['article-link']}>
+      <div className={classNames(this.props.className, stylesArticleLink['article-link'])}>
         <Link to={prefixLink(path)} title={name}  className={stylesArticleLink['title']}>{name}</Link>
       {description
         && <Link to={prefixLink(path)} title={name} className={stylesArticleLink['description']}>{description}</Link>
--- components/ArticleLinks.js
+++ components/ArticleLinks.js
@@ -1,9 +1,19 @@
 import React, {Component, PropTypes} from 'react';
+const classNames = require('classnames');
 const sortBy = require('lodash.sortby');
 import ArticleLink from './ArticleLink';
+const expansionThreshold = 8;
+import stylesContentList from './content-list.module.scss';
 
 export default class ProjectLinks extends Component {
 
+  constructor(props) {
+
+    super(props);
+    this.state = {
+      showExpanded: props.showExpanded ? true : false
+    };
+  }
 
   static get propTypes() {
 
@@ -17,14 +27,34 @@ export default class ProjectLinks extends Component {
 
     //get all pages we will render (should be pre-filtered)
     const { pages } = this.props;
-    //page sort order
+    //sort pages to order and categories
     const sortedPages = sortBy(pages, (page) => page.data.created_utc || 0).reverse();
+    const displayedPages = this.state.showExpanded ? sortedPages : sortedPages.slice(0,expansionThreshold);
+    const hiddenPages = this.state.showExpanded ? [] : sortedPages.slice(expansionThreshold);
     //convert to elements
     return (
-      <div className="article-links">
-        {sortedPages.map(page => <ArticleLink key={page.path} name={page.data.title} path={page.path} description={page.data.description} />)}
+      <div className={stylesContentList['content-list']}>
+
+        {displayedPages.map(page => <ArticleLink className={stylesContentList.item} key={page.path} name={page.data.title} path={page.path} description={page.data.description} />)}
+
+        {hiddenPages.map(page => <ArticleLink className={classNames(stylesContentList.item, stylesContentList.overThreshold)} key={page.path} name={page.data.title} path={page.path} description={page.data.description} />)}
+
+        <button type="button" className={classNames(
+          stylesContentList['expand-command'], {
+          [stylesContentList['-is-expanded']]: this.state.showExpanded
+        })} onClick={() => this.toggleExpanded()}>Show {this.state.showExpanded ? "fewer" : "more"} articles</button>
+
       </div>
     );
+  }
+
+
+  toggleExpanded() {
+
+    this.setState({
+      ...this.state,
+      showExpanded: !this.state.showExpanded
+    });
   }
 
 
