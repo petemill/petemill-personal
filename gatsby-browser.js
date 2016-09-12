9e4befbfe83a26fdc3007c0541424dd162b5d805
Pete Miller <miller.pete+commit@gmail.com>
2016-09-12 14:12:13 +0100

    fix: ga tracking script include

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,13 +1,20 @@
 //tracking script is only loaded for production
 if (process.env.NODE_ENV === 'production') {
-  // fire for every 'page' change
-  exports.onRouteChange = (state, page, pages) => {
+  //pasted GA init script
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  //init GA account
+  ga('create', 'UA-84020551-1', 'auto');
+  // fire for every 'page' change (including current)
+  exports.onRouteUpdate = (state, page, pages) => {
 
     //make sure GA is loaded
     if (ga) {
       //send pageview hit for current path
       ga('send', 'pageview', {
-        page: state.path,
+        page: state.pathname,
       })
     }
   }
--- html.js
+++ html.js
@@ -22,18 +22,6 @@ export default class Root extends Component {
       css = <style dangerouslySetInnerHTML={{ __html: require('!raw!./public/styles.css') }} />
     }
 
-    let tracking;
-    if (process.env.NODE_ENV === 'production') {
-      tracking = <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-        ga('create', 'UA-84020551-1', 'auto');
-        ga('send', 'pageview');
-      </script>
-    }
-
     return (
       <html lang="en">
         <head>
@@ -48,10 +36,9 @@ export default class Root extends Component {
           {css}
           <link href="https://fonts.googleapis.com/css?family=Work+Sans|Space+Mono|VT323" rel="stylesheet" />
         </head>
-        <body>  
+        <body>
           <div id="react-mount" dangerouslySetInnerHTML={{ __html: this.props.body }} />
           <script src={prefixLink(`/bundle.js?t=${BUILD_TIME}`)} />
-          {tracking}
         </body>
       </html>
     )
